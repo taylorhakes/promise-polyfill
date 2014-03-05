90f57ab76d75b99fcef8f5751981be540abc6791
Taylor Hakes <taylor@taylorhakes.com>
2014-03-05 00:22:50 -0500

    Removed PHP storm comments

--- tests/adapter.js
+++ tests/adapter.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: taylorhakes
- * Date: 3/4/14
- * Time: 11:15 PM
- * To change this template use File | Settings | File Templates.
- */
 var Promise = require('../Promise');
 module.exports = {
 	resolved: Promise.resolve,
