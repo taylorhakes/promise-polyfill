408b60caa3e5b2809573f0af12dcc9f4d6f4427b
taylorhakes <taylor.hakes@gmail.com>
2015-06-24 23:50:37 -0400

    Added additional immediateFn check

--- tests/Promise.spec.js
+++ tests/Promise.spec.js
@@ -9,10 +9,14 @@ describe('Promise', function() {
 				fn();
 			});
 			Promise._setImmediateFn(spy);
-			var a = new Promise(function(resolve, reject) {
+			var done = false,
+				a = new Promise(function(resolve, reject) {
 				resolve();
-			}).then(function() {});
+			}).then(function() {
+				done = true;
+			});
 			expect(spy).toHaveBeenCalled();
+			expect(done).toBe(true);
 		});
 		it('changes immediate fn multiple', function() {
 			var spy1 = jasmine.createSpy('callback').and.callFake(function(fn) {
@@ -22,15 +26,20 @@ describe('Promise', function() {
 				fn();
 			});
 			Promise._setImmediateFn(spy1);
-			var a = new Promise(function(resolve, reject) {
+			var done = false,
+				a = new Promise(function(resolve, reject) {
 				resolve();
 			}).then(function() {});
 			Promise._setImmediateFn(spy2);
-			var a = new Promise(function(resolve, reject) {
+
+			a = new Promise(function(resolve, reject) {
 				resolve();
-			}).then(function() {});
+			}).then(function() {
+				done = true;
+			});
 			expect(spy2).toHaveBeenCalled();
 			expect(spy1.calls.count()).toBe(1);
+			expect(done).toBe(true);
 		});
 	});
 });
