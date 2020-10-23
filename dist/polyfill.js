46c6448585b270d4e7622a562afe587394b5ad89
Taylor Hakes <taylorhakes@users.noreply.github.com>
2020-10-23 14:03:54 -0700

    Update compiled files

--- dist/polyfill.js
+++ dist/polyfill.js
@@ -28,39 +28,50 @@ function finallyConstructor(callback) {
 
 function allSettled(arr) {
   var P = this;
-  return new P(function(resolve,reject) {
-    if(!Boolean(arr && typeof arr.length !== 'undefined')) {
-      return reject(new TypeError(typeof arr + ' ' + arr + ' is not iterable(cannot read property Symbol(Symbol.iterator))'));
+  return new P(function(resolve, reject) {
+    if (!(arr && typeof arr.length !== 'undefined')) {
+      return reject(
+        new TypeError(
+          typeof arr +
+            ' ' +
+            arr +
+            ' is not iterable(cannot read property Symbol(Symbol.iterator))'
+        )
+      );
     }
     var args = Array.prototype.slice.call(arr);
     if (args.length === 0) return resolve([]);
     var remaining = args.length;
 
     function res(i, val) {
-      if(val && (typeof val === 'object' || typeof val === 'function')) {
+      if (val && (typeof val === 'object' || typeof val === 'function')) {
         var then = val.then;
-        if(typeof then === 'function'){
-          then.call(val,function(val) {
-            res(i, val);
-          }, function(e) {
-            args[i] = { status: 'rejected', reason: e };
-            if(--remaining === 0) {
-              resolve(args);
+        if (typeof then === 'function') {
+          then.call(
+            val,
+            function(val) {
+              res(i, val);
+            },
+            function(e) {
+              args[i] = { status: 'rejected', reason: e };
+              if (--remaining === 0) {
+                resolve(args);
+              }
             }
-          });
+          );
           return;
         }
       }
       args[i] = { status: 'fulfilled', value: val };
-      if(--remaining === 0) {
+      if (--remaining === 0) {
         resolve(args);
       }
     }
 
-    for(var i = 0; i < args.length; i++){
+    for (var i = 0; i < args.length; i++) {
       res(i, args[i]);
     }
-  })
+  });
 }
 
 // Store setTimeout reference so promise-polyfill will be unaffected by
